d89fc0e6cc75a4bfb367194ce938bdf0d4a1f827
fahad <aungsoelin@my.jcu.edu.au>
2020-12-04 03:54:32 +0630

    add more functions

--- prac04/subject_reader.py
+++ prac04/subject_reader.py
@@ -15,13 +15,10 @@ def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         part = line.split(',')  # Separate the data into its parts
-        print(part)  # See what the parts look like (notice the integer is a string)
         part[2] = int(part[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(part)  # See if that worked
+        parts=[]
         parts.append(part)
         print(parts)
         print("----------")
