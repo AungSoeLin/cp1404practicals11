49a47d69bd464f6f99753b413ccde2650425e6fb
fahad <aungsoelin@my.jcu.edu.au>
2020-12-04 03:35:21 +0630

    finish file

--- prac04/list_exercises.py
+++ prac04/list_exercises.py
@@ -5,3 +5,11 @@ print("The Smallest Number is",min(numbers))
 print("The Largest Number is",max(numbers))
 average = sum(numbers)/5
 print("The average of the numbers is",average)
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+name = input("Enter your name:")
+if name in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
