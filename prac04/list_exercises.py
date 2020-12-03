bb7f93f428eb9eb062ff3fe1f1d9612f206aac4b
fahad <aungsoelin@my.jcu.edu.au>
2020-12-04 03:32:10 +0630

    add functions

---
+++ prac04/list_exercises.py
@@ -0,0 +1,7 @@
+numbers=[5,20,1,2,3]
+print("The First Number is",numbers[0])
+print("The Last Number is",numbers[4])
+print("The Smallest Number is",min(numbers))
+print("The Largest Number is",max(numbers))
+average = sum(numbers)/5
+print("The average of the numbers is",average)
--- prac04/subject_reader.py
+++ prac04/subject_reader.py
@@ -18,10 +18,12 @@ def get_data():
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        part = line.split(',')  # Separate the data into its parts
+        print(part)  # See what the parts look like (notice the integer is a string)
+        part[2] = int(part[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(part)  # See if that worked
+        parts.append(part)
+        print(parts)
         print("----------")
     input_file.close()
 
